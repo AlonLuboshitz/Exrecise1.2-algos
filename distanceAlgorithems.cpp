1f778b24e2641e91c3cfd5e8ff8ed3e303f3c03f
GiliWolf <giliwolf1999@gmail.com>
2022-11-19 18:06:15 +0200

    main is finnished after testing. didnt touch outher files.

--- distanceAlgorithems.cpp
+++ distanceAlgorithems.cpp
@@ -1,24 +1,24 @@
 
-#include "mainHeader.h"
-#include <cmath>
+// #include "mainHeader.h"
+// #include <cmath>
 
 
-distanceAlgorithems::distanceAlgorithems(const std::vector<double>& vec1, const std::vector<double>& vec2) {
+// distanceAlgorithems(const std::vector<double>& vec1, const std::vector<double>& vec2) {
 	
-	m_vec1 = vec1;
-	m_vec2 = vec2;
-	//diffVec = { 0 };
-	m_diffVec = m_vecMan.deductionF(m_vec1, m_vec2);
-}
+// 	m_vec1 = vec1;
+// 	m_vec2 = vec2;
+// 	//diffVec = { 0 };
+// 	m_diffVec = m_vecMan.deductionF(m_vec1, m_vec2);
+// }
 
-double distanceAlgorithems::euclideanDistance() {
-double sum;
-std::vector<double>::iterator iter = m_diffVec.begin();
-	for (iter; iter != m_diffVec.end(); ++iter) {
-		sum += (*iter) * (*iter);
-	}
-	return sqrt(sum);
-}
+// double euclideanDistance() {
+// double sum;
+// std::vector<double>::iterator iter = m_diffVec.begin();
+// 	for (iter; iter != m_diffVec.end(); ++iter) {
+// 		sum += (*iter) * (*iter);
+// 	}
+// 	return sqrt(sum);
+// }
 
 
 
