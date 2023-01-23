f447f39718cf2406f944a732f0543495d5bcb4bf
GiliWolf <giliwolf1999@gmail.com>
2023-01-23 18:22:54 +0200

    socketIO

--- SocketIO.h
+++ SocketIO.h
@@ -1,18 +1,19 @@
-#ifndef DEFAULTIO_H
-#define DEFAULTIO_H
+#ifndef SOCKETIO_H
+#define SOCKETIO_H
 #include "defualtIO.h"
 #include <sys/socket.h>
 #include <string.h>
 #include <iostream>
 #include <cstring>
 #include <stdio.h>
-#include <ostream>
+#include <fstream>  
 class SocketIO : public defualtIO
 {
 private:
     /* data */
      int m_client_socket_fd;
      int m_expected_data_length;
+     std::string m_messegeLeft; 
 public:
     SocketIO() = delete;
     SocketIO(int client_socket_fd, int expected_data_length);
@@ -21,7 +22,9 @@ public:
     void write(std::string str);
     std::string read();
     bool sendMessage(std::string message);
-    bool getMessage(char buffer[]);
+    int getMessage(char buffer[]);
+    int endOfMsg(char* recievedMessege, int recievedBytes);
+    void resetMsg(char* recievedMessege, int endOfMsg, int recievedBytes);
 };
 
 
