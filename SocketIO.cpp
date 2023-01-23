baca518f6981c764b619094453f0c0d103417e2a
GiliWolf <giliwolf1999@gmail.com>
2023-01-23 15:45:34 +0200

    added seperate line and reset messege

--- SocketIO.cpp
+++ SocketIO.cpp
@@ -19,6 +19,54 @@ bool SocketIO::sendMessage(std::string message) {
     }
     else return true;
 }
+int endOfMsg(char* recievedMessege, int recievedBytes){
+    std::string stop;
+    int i;
+    for (i = 0; i < recievedBytes; i++){
+        if(*(recievedMessege+i) == 'x'){
+            stop.append("x");
+            if (stop == "xxx"){
+                break;
+            }
+        }
+    }
+    return i;
+    
+}
+
+void resetMsg(char* recievedMessege, int endOfMsg, int buffer){
+    if (endOfMsg == buffer){
+        return;
+    }
+    std::memmove(recievedMessege, recievedMessege + endOfMsg + 1, buffer - endOfMsg);
+    *(recievedMessege + endOfMsg +1) = '\0';
+ }
+
+ void seperateLines(std::ofstream& file,char* recievedMessege,  int recievedBytes){
+    std::string tempWord;
+    for (int j = 0; j < recievedBytes; j++){
+        if (*(recievedMessege+j) == ' '){
+            file<<tempWord;
+            file<<",";
+            tempWord.clear();
+        }
+        else if (*(recievedMessege+j) == '\\' && *(recievedMessege+j + 1) == 'n'){
+            file<<tempWord;
+            file<<"\n";
+            tempWord.clear();
+            j++;
+        }
+
+        else {
+            std::string tempChar = std::string(1, *(recievedMessege +j));
+            tempWord.append(tempChar);
+            tempChar.clear();
+        
+        }
+    }
+    file<<tempWord;
+    file<<",";
+ }
 
 bool SocketIO::getMessage( char buffer[]) {
     int read_bytes = recv(m_client_socket_fd,buffer,m_expected_data_length,0);
@@ -41,7 +89,7 @@ void SocketIO::write(std::string str){
 }
 
 std::string read(){
-    
+
 }
 
 SocketIO::~SocketIO()
--- SocketIO.h
+++ SocketIO.h
@@ -4,6 +4,9 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <iostream>
+#include <cstring>
+#include <stdio.h>
+#include <ostream>
 class SocketIO : public defualtIO
 {
 private:
